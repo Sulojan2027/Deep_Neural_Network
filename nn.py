b08882947a9c703e79e86304325b465e379482e2
Sulojan <sulojanrajkumar@gmail.com>
2026-08-20 16:29:06 +0530

    feat: Added expanded DNN

--- nn.py
+++ nn.py
@@ -1,38 +1,79 @@
-class MyNN:
-    def __init__(self):
-        self.w1 = 1
-        self.w2 = 1
-        self.b = 0
+import numpy as np
 
-    def fit(self, X, y, learning_rate, epochs, loss_threshold):
-        self.w1, self.w2, self.b = self._gradient_descent(X['age'], X['affordibility'], y, learning_rate, epochs, loss_threshold)
-        print(f"Final weights and bias: w1:{self.w1}, w2: {self.w2}, bias: {self.b}")
+class DeepNN:
+    """Deep Neural Network classifier
+    Params:
+        layer_dims: list of integers, number of neurons in each layer
+        mid_activation: activation function for hidden layers
+        mid_activation_backward: derivative of mid_activation
+        out_activation: activation function for output layer
+    """
+    def __init__(self, layer_dims, mid_activation, mid_activation_backward, out_activation):
+        self.parameters = {}
+        self.num_layers = len(layer_dims) - 1
+        self.mid_activation = mid_activation
+        self.mid_activation_backward = mid_activation_backward
+        self.out_activation = out_activation
 
-    def predict(self,x_test):
-        weighted_sum = self.w1 * x_test['age'] + self.w2 * x_test['affordibility'] + self.b
-        return sigmoid_numpy(weighted_sum)
-    
-    def _gradient_descent(self, x1, x2, y, learning_rate, epochs, loss_threshold):
-        w1 = w2 = 1
-        b = 0
-        n = len(x1)
-        for i in range(epochs):
-            weight = w1 * x1 + w2 * x2 + b
-            y_hat = sigmoid_numpy(weight)
-            loss  = sigmoid_loss(y, y_hat)
+        for i in range(1, self.num_layers + 1):
+            self.parameters['W' + str(i)] = np.random.randn(layer_dims[i], layer_dims[i-1]) * 0.1
+            self.parameters['b' + str(i)] = np.zeros((layer_dims[i], 1))
 
-            w1d = np.dot(x1.T, y - y_hat) / n
-            w2d = np.dot(x2.T, y - y_hat) / n
+    def forward_propagation(self, X):
+        caches = {}
+        A = X
+        caches['A0'] = X
+        for i in range(1, self.num_layers + 1):
+            W = self.parameters['W' + str(i)]
+            b = self.parameters['b' + str(i)]
+            Z = np.dot(W, A) + b
+            if i == self.num_layers:
+                A = self.out_activation(Z)
+            else:
+                A = self.mid_activation(Z)
+            caches['Z' + str(i)] = Z
+            caches['A' + str(i)] = A
 
-            b = np.sum(y - y_hat) / n
-            w1 = w1 - learning_rate * w1
-            w2 = w2 - learning_rate * w2
-            b = b - learning_rate * b
+        return A, caches
+
+    def backward_propagation(self, X, Y, caches):
+        grads = {}
+        m = X.shape[1]
+
+        # Last layer
+        Z = caches['Z' + str(self.num_layers)]
+        A = caches['A' + str(self.num_layers)]
+        dZ = A - Y
+        dW = (1/m) * np.dot(dZ, caches['A' + str(self.num_layers-1)].T)
+        db = (1/m) * np.sum(dZ, axis=1, keepdims=True)
+        grads['dW' + str(self.num_layers)] = dW
+        grads['db' + str(self.num_layers)] = db
+
+        for i in range(self.num_layers-1, 0, -1):
+            Z = caches['Z' + str(i)]
+            A_prev = caches['A' + str(i-1)]
+            W_next = self.parameters['W' + str(i+1)]
             
-            if i % 10 == 0:
-                print(f'Epoch: {i}, w1: {w1}, w2: {w2}, bias: {b}, Loss: {loss}')
+            dZ = np.dot(W_next.T, dZ) * self.mid_activation_backward(Z)
+            dW = (1/m) * np.dot(dZ, A_prev.T)
+            db = (1/m) * np.sum(dZ, axis=1, keepdims=True)
+            grads['dW' + str(i)] = dW
+            grads['db' + str(i)] = db
+        
+        return grads
 
-            if loss < loss_threshold:
-                break
+    def update_parameters(self, grads, learning_rate):
+        """Update parameters"""
+        for i in range(1, self.num_layers + 1):
+            self.parameters['W' + str(i)] -= learning_rate * grads['dW' + str(i)]
+            self.parameters['b' + str(i)] -= learning_rate * grads['db' + str(i)]
 
-        return w1, w2, b
+    def predict(self, X):
+        """Predict output
+        Params:
+            X: input data
+        Returns:
+            predicted output
+        """
+        A, _ = self._forward_propagation(X)
+        return A
