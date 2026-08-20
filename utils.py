3e6e66e2a0d7ae59db443ee2633abf1c30596669
Sulojan <sulojanrajkumar@gmail.com>
2026-08-20 16:36:21 +0530

    feat: Utilities needed

--- utils.py
+++ utils.py
@@ -1,10 +1,27 @@
-def sigmoid_numpy(z):
+import numpy as np
+
+def sigmoid(z):
     return 1 / (1 + np.exp(-z))
 
-def sigmoid_loss(y, y_hat):
+def relu(z):
+    return np.maximum(0, z)
+
+def relu_backward(z):
+    return (z > 0).astype(float)
+
+def softmax(z):
+    exp_z = np.exp(z - np.max(z, axis=0, keepdims=True))
+    return exp_z / np.sum(exp_z, axis=0, keepdims=True)
+
+def binary_crossentropy(y, y_hat):
     epsilon = 1e-15
-    y_hat_new = [max(i, epsilon) for i in y_hat]
-    y_hat_new = [min(i, 1-epsilon) for i in y_hat_new]
-    y_hat_new = np.array(y_hat_new)
+    y_hat_new = np.clip(y_hat, epsilon, 1 - epsilon)
     return -np.mean(y * np.log(y_hat_new) + (1 - y) * np.log(1 - y_hat_new))
-    # return -np.mean(y_true*np.log(y_predicted_new)+(1-y_true)*np.log(1-y_predicted_new))
+
+def categorical_crossentropy(Y, Y_hat):
+    epsilon = 1e-15
+    Y_hat_clipped = np.clip(Y_hat, epsilon, 1 - epsilon)
+    return -np.sum(Y * np.log(Y_hat_clipped)) / Y.shape[1]
+
+def one_hot_encode(Y, num_classes):
+    return np.eye(num_classes)[Y].T
